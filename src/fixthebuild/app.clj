d52f35ee5e65a76fc22cbc0909216c3c338e8a70
Jeroen Hoekx <jeroen@hoekx.be>
2017-02-09 20:43:15 +0100

    Fix index.html content type

--- src/fixthebuild/app.clj
+++ src/fixthebuild/app.clj
@@ -2,12 +2,13 @@
   (:require [integrant.core :as ig]
             [compojure.core :refer :all]
             [ring.middleware.defaults :refer :all]
-            [ring.util.response :refer [resource-response]]))
+            [ring.util.response :refer [header resource-response]]))
 
 (defroutes
   app
   (GET "/" []
-    (resource-response "index.html" {:root "public"})))
+    (-> (resource-response "index.html" {:root "public"})
+        (header "Content-Type" "text/html"))))
 
 (defn- make-handler []
   (wrap-defaults app site-defaults))
